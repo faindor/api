2b10b6ce1cdefd886217130ca099df77d09b1d89
gbbelloponce <gbbelloponce@gmail.com>
2024-05-27 14:02:09 -0300

    Move db folder to shared

--- src/config/db/schema/organization.ts
+++
@@ -1,8 +0,0 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
-
-export const organization = pgTable("organization", {
-	id: serial("id").primaryKey(),
-	domain: text("domain").notNull(),
-	createdAt: timestamp("created_at").notNull().defaultNow(),
-	deletedAt: timestamp("deleted_at").notNull().defaultNow(),
-});
