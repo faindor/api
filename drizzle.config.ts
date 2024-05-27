b7c0381547fa1ace5f37673cf03693433ec51bc5
gbbelloponce <gbbelloponce@gmail.com>
2024-05-27 15:40:12 -0300

    fix: Modify db folder path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
 	dialect: "postgresql",
-	schema: "./src/config/db/schema/*",
-	out: "./src/config/db/migrations",
+	schema: "./src/shared/db/schema/*",
+	out: "./src/shared/db/migrations",
 	dbCredentials: {
 		url: process.env.DATABASE_URL,
 	},
