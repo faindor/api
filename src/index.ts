220330e334beef03f52276b76e012371c6e8005d
gbbelloponce <gbbelloponce@gmail.com>
2024-05-28 01:05:57 -0300

    feat: Add common middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { prettyJSON } from "hono/pretty-json";
 
 import { applyRoutes } from "./applyRoutes";
 
 const app = new Hono();
+app.use(cors());
+app.use(prettyJSON());
 
 applyRoutes(app);
 
