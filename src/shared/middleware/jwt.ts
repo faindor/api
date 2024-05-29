b704199200c54e4c51918fe18e342c7f807dae70
gbbelloponce <gbbelloponce@gmail.com>
2024-05-29 00:59:55 -0300

    types: Add new error types and context variables in middleware

--- src/shared/middleware/jwt.ts
+++ src/shared/middleware/jwt.ts
@@ -1,11 +1,17 @@
+import { AuthorizationError } from "@shared/types/errors";
 import { createMiddleware } from "hono/factory";
 import { verify } from "hono/jwt";
 
-export const jwt = createMiddleware(async (c, next) => {
+export const jwt = createMiddleware<{
+	Variables: {
+		userId: number;
+		userDomain: string;
+	};
+}>(async (c, next) => {
 	const authorizationHeader = c.req.header("Authorization");
 	if (!authorizationHeader) {
 		return c.json(
-			{ error: "No authorization header provided" },
+			{ error: new AuthorizationError("No authorization header provided") },
 			{ status: 401 },
 		);
 	}
@@ -13,14 +19,21 @@ export const jwt = createMiddleware(async (c, next) => {
 	// Get the token
 	const [, token] = authorizationHeader.split(" ");
 	if (!token) {
-		return c.json({ error: "No token provided" }, { status: 401 });
+		return c.json(
+			{ error: new AuthorizationError("No token provided") },
+			{ status: 401 },
+		);
 	}
 
 	try {
 		const payload = await verify(token, Bun.env.JWT_SECRET);
-		c.set("jwtPayload", payload);
+		c.set("userId", payload.userId as number);
+		c.set("userDomain", payload.userDomain as string);
 	} catch (error) {
-		return c.json({ error: "Invalid token" }, { status: 401 });
+		return c.json(
+			{ error: new AuthorizationError("Invalid token") },
+			{ status: 401 },
+		);
 	}
 
 	await next();
---
+++ src/shared/types/errors.ts
@@ -0,0 +1,6 @@
+export class NotFoundError extends Error {}
+export class CouldNotCreateError extends Error {}
+export class CouldNotUpdateError extends Error {}
+export class InvalidPasswordError extends Error {}
+export class InvalidPayloadError extends Error {}
+export class AuthorizationError extends Error {}
