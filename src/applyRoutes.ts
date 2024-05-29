dcacd50e704ff9e955c2ccd0f7ce491c6fc15030
gbbelloponce <gbbelloponce@gmail.com>
2024-05-28 22:33:49 -0300

    refactor: remove applyRoutes.ts

--- src/applyRoutes.ts
+++
@@ -1,7 +0,0 @@
-import type { Hono } from "hono";
-
-import auth from "@modules/auth";
-
-export const applyRoutes = (app: Hono) => {
-	app.route("/auth", auth);
-};
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { prettyJSON } from "hono/pretty-json";
 
-import { applyRoutes } from "./applyRoutes";
+import auth from "@modules/auth";
 
 const app = new Hono();
 app.use(cors());
 app.use(prettyJSON());
 
-applyRoutes(app);
+app.route("/auth", auth);
 
 export default app;
