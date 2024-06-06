ff89dde597d5fd544e9126107b7a1aab2fd19941
gbbelloponce <gbbelloponce@gmail.com>
2024-06-06 00:17:08 -0300

    refactor: wrap jwt middleware inside try/catch statement

--- src/shared/middleware/jwt.ts
+++ src/shared/middleware/jwt.ts
@@ -2,7 +2,7 @@ import { AuthorizationError } from "@shared/types/errors";
 import { createMiddleware } from "hono/factory";
 import { verify } from "hono/jwt";
 
-export const jwt = createMiddleware<{
+interface JWTMiddleware {
 	Variables: {
 		loggedUser: {
 			id: number;
@@ -10,37 +10,36 @@ export const jwt = createMiddleware<{
 			domain: string;
 		};
 	};
-}>(async (c, next) => {
-	const authorizationHeader = c.req.header("Authorization");
-	if (!authorizationHeader) {
-		return c.json(
-			{ error: new AuthorizationError("No authorization header provided") },
-			{ status: 401 },
-		);
-	}
+}
 
-	// Get the token
-	const [, token] = authorizationHeader.split(" ");
-	if (!token) {
-		return c.json(
-			{ error: new AuthorizationError("No token provided") },
-			{ status: 401 },
-		);
-	}
-
+export const jwt = createMiddleware<JWTMiddleware>(async (c, next) => {
 	try {
+		const authorizationHeader = c.req.header("Authorization");
+		if (!authorizationHeader) {
+			throw new AuthorizationError("No authorization header provided");
+		}
+
+		// Get the token
+		const [, token] = authorizationHeader.split(" ");
+		if (!token) {
+			throw new AuthorizationError("No token provided");
+		}
+
+		// Verify and valiadte the token
 		const payload = await verify(token, Bun.env.JWT_SECRET);
 
 		if (!payload.userId || !payload.userDomain || !payload.userRole) {
 			throw new AuthorizationError("Invalid token");
 		}
 
+		// Set the user's token info in the context
 		c.set("loggedUser", {
 			id: payload.userId as number,
 			role: payload.userRole as string,
 			domain: payload.userDomain as string,
 		});
 	} catch (error) {
+		console.error(error);
 		return c.json({ error }, { status: 401 });
 	}
 
