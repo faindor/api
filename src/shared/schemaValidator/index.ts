d453cff52f1d5631d9ab383f84063322097ba765
gbbelloponce <gbbelloponce@gmail.com>
2024-06-10 22:40:31 -0300

    feat: Add schema validator util

---
+++ src/shared/schemaValidator/index.ts
@@ -0,0 +1,24 @@
+import type { z } from "zod";
+import { ValidationError } from "@shared/types/errors";
+
+type SchemaValidatorParams<T extends z.ZodType> = {
+	schema: T;
+	value: unknown;
+	route: string;
+};
+
+export const schemaValidator = <T extends z.ZodType>({
+	schema,
+	value,
+	route,
+}: SchemaValidatorParams<T>): z.infer<T> => {
+	const { success, data, error } = schema.safeParse(value);
+
+	if (success) {
+		return data;
+	}
+
+	console.error(error);
+
+	throw new ValidationError(`Failed to validate the request at ${route}`);
+};
